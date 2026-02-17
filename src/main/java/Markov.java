5c45c8c84dd150e6170b727ce0bce7fd766f5ae5
vllacuna <vllacuna@csumb.edu>
2026-02-17 11:06:34 -0800

    UML Addresses

---
+++ src/main/java/Markov.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Markov {
+    private static String BEGINS_SENTENCE = "__$";
+    private static String PUNCTUATION_MARKS = ".!?$";
+    private HashMap<String, ArrayList<String>> words;
+    private String prevWord;
+
+    public Markov() {
+
+    }
+
+    public String getSentence() {
+
+    }
+
+    public void addFromFile(String str) {
+
+    }
+
+    void addWord(String str) {
+
+    }
+
+    String randomWord(String str) {
+
+    }
+
+    public String toString() {
+
+    }
+
+    HashMap<String, ArrayList<String>> getWords() {
+
+    }
+
+    void addLine(String str) {
+
+    }
+
+    public static boolean endsWithPunctuation(String str) {
+
+    }
+}
